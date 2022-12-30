695e5ed99f5aa68ddc4755c82269933be2e03303
Johnny Proano <djimageonline@gmail.com>
2022-12-29 19:02:43 -0500

    initial commit and added user, brewery, tour models

---
+++ db/migrate/20221229235842_create_breweries.rb
@@ -0,0 +1,11 @@
+class CreateBreweries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :breweries do |t|
+      t.string :name
+      t.string :address
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20221229235923_create_tours.rb
@@ -0,0 +1,10 @@
+class CreateTours < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tours do |t|
+      t.integer :user_id
+      t.integer :brewery_id
+
+      t.timestamps
+    end
+  end
+end
