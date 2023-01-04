0acf90cd0f9a82aa4a2d79acd2ecc0c3e23b3d13
Johnny Proano <djimageonline@gmail.com>
2023-01-03 22:06:34 -0500

    defined brewery tour createa method

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,28 +1,25 @@
 class BreweriesController < ApplicationController
 
   def index
-    # @brewery = Brewery.all
-    # render json: @brewery.as_json
+    @brewery = Brewery.all
+    render json: @brewery.as_json
   
+    #params
+    # city = "tampa"
+    # state = "fl"
 
-    # response = HTTP.get("http://beermapping.com/webservice/loccity/#{ENV['Brew_Key']}/tampa,fl&s=json")
+    # response = HTTP.get("http://beermapping.com/webservice/loccity/#{Rails.application.credentials.beer_api[:api_key]}/#{city},#{state}&s=json")
 
-      #params
-    city = "orlando"
-    state = "fl"
+    # show = response.parse(:json)
 
-    response = HTTP.get("http://beermapping.com/webservice/loccity/#{Rails.application.credentials.beer_api[:api_key]}/#{city},#{state}&s=json")
+    # index = 0
+    # brewery_names = []
+    # while index < show.length
+    #   brewery_names << show[index]["name"]
+    #   index += 1
+    # end
 
-    show = response.parse(:json)
-
-    index = 0
-    brewery_names = []
-    while index < show.length
-      brewery_names << show[index]["name"]
-      index += 1
-    end
-
-    render json:  show.as_json
+    # render json:  show.as_json
   end
 
 
--- app/controllers/breweries_tours_controller.rb
+++ app/controllers/breweries_tours_controller.rb
@@ -1,7 +1,5 @@
 class BreweriesToursController < ApplicationController
 
-
-
   def index
     @brewery_tour = BreweryTour.all
     render json: @brewery_tour
@@ -13,9 +11,9 @@ class BreweriesToursController < ApplicationController
     #Step1: locate brewery in database
     #Step2: if you dont find brewery , create that brewery in db
     #Step3: use brewery id (wether you found it or created it)  ----- These are done
-
     #Step4: get tours that belong to current user :name
-    
+    #Step5: create the brewery tour entry: 
+
     brewery = Brewery.find_by(
       name: params[:name]
     )
@@ -31,30 +29,29 @@ class BreweriesToursController < ApplicationController
       brewery.save
     end
 
-    pp brewery
-  
-
-
-    # @brewery_tour = BreweryTour.new(
-    #   brewery_id:params[:brewery_id],
-    #   tour_id:params[:tour_id]
-
-    # )
+    tour = Tour.find_by(
+      user_id:current_user.id,
+      name: params[:tour_name]
+    )
     
-    # if @brewery_tour.save
-    #   render json: @brewery_tour
-    # else
-    #   render json: { errors: @brewery_tour.errors.full_messages }, status: 422
-    # end
+    brewery_tour = BreweryTour.new(
+      user_id: current_user.id,
+      brewery_id: brewery.id,
+      tour_id: tour.id
+    )
+    
+    if brewery_tour.save
+      render json: brewery_tour.as_json
+    else
+      render json: { errors: brewery_tour.errors.full_messages }, status: 422
+    end
   end
+
 
   def show
     @brewery_tour = BreweryTour.find_by(id: params[:id])
     render json: @brewery_tour
   end
-
-
-
 
 
 end
