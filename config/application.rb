52a00883da5d687c4adc6facbe3138b0adf98fa1
Johnny Proano <jproano22@yahoo.com>
2023-02-13 22:23:00 -0500

    rails db drop, creaste and migrate

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "action_cable/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+#
 module Brewr
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
