c0d8f54c0b63f604f2d03ebae6088f3e3e8fb259
Johnny Proano <jproano22@yahoo.com>
2023-02-27 19:38:51 -0500

    correct migration in db migration mispelling

--- db/migrate/20221230042814_change_breweries_and_tours_id.rb
+++ db/migrate/20221230042814_change_breweries_and_tours_id.rb
@@ -1,6 +1,6 @@
 class ChangeBreweriesAndToursId < ActiveRecord::Migration[7.0]
   def change
-    remove_column :brewery_tour, :breweries_id, :integer
+    remove_column :brewery_tours, :breweries_id, :integer
     remove_column :brewery_tours, :tours_id, :integer
     add_column :brewery_tours, :brewery_id, :integer
     add_column :brewery_tours, :tour_id, :integer
